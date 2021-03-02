b7256fbfdc477a31cf40600cdb2d012a26c4fd2e
John Bielejeski <jmbielejeski@gmail.com>
2021-03-02 10:51:10 -0700

    finished base mode

---
+++ joins-solutions.sql
@@ -0,0 +1,54 @@
+-- Base
+
+-- 1. Get all customers and their addresses.
+SELECT * FROM "customers"
+JOIN "addresses" ON "addresses".customer_id = "customers".id;
+
+-- 2. Get all orders and their line items (orders, quantity and product).
+SELECT * FROM "warehouse"
+JOIN "warehouse_product" ON "warehouse_product".warehouse_id = "warehouse".id
+JOIN "products" ON "products".id = "warehouse_product".product_id
+WHERE "products".id = 5;
+
+-- 3. Which warehouses have cheetos?
+SELECT * FROM "warehouse"
+JOIN "warehouse_product" ON "warehouse_product".warehouse_id = "warehouse".id
+JOIN "products" ON "products".id = "warehouse_product".product_id
+WHERE "products".id = 5;
+-- 4. Which warehouses have diet pepsi?
+SELECT * FROM "warehouse"
+JOIN "warehouse_product" ON "warehouse_product".warehouse_id = "warehouse".id
+JOIN "products" ON "products".id = "warehouse_product".product_id
+WHERE "products".id = 6;
+
+-- 5. Get the number of orders for each customer. NOTE: It is OK if those without orders are not included in results.
+SELECT
+	"customers".first_name,
+	count("orders".id)
+FROM "customers"
+JOIN "addresses" ON "addresses".customer_id = "customers".id
+JOIN "orders" ON "orders".address_id = "addresses".id
+GROUP BY "customers".first_name;
+
+-- 6. How many customers do we have?
+SELECT count("customers".id) FROM "customers";
+
+-- 7. How many products do we carry?
+SELECT SUM("warehouse_product".product_id) FROM "warehouse_product";
+
+-- 8. What is the total available on-hand quantity of diet pepsi?
+SELECT 
+	"products".description,
+	SUM("warehouse_product".on_hand) 
+FROM "warehouse_product"
+JOIN "products" ON "products".id = "warehouse_product".product_id
+WHERE "products".id = 6
+GROUP BY "products".description;
+
+
+-- Stretch
+-- 9. How much was the total cost for each order?
+
+-- 10. How much has each customer spent in total?
+
+-- 11. How much has each customer spent in total? Customers who have spent $0 should still show up in the table. It should say 0, not NULL (research coalesce).
